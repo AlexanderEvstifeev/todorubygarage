7c67aff34686e94d491c18f41bef6a868e7a3d85
AlexanderEvstifeev <chaosgum12@gmail.com>
2019-09-20 15:18:57 +0300

    gotovo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -9,39 +9,55 @@ const bodyParser = require('body-parser');
     });
 
     router.post('/new-task', (req, res, next) =>{
-        console.log('Request Type:', req.method);
-        req.accepts('application/json')
+
         let idTask = req.body.key;
         let textTitle = req.body.Title;
         let obj = {
             Title: textTitle,
             project_id: idTask,
-            status: 'true'
+            status: 'false'
         };
-
+        if((textTitle === 0 || textTitle.trim())){
         let sql = 'INSERT INTO tasks SET ?';
         let query = db.query(sql, [obj], (err, rows, fields) => {
             if (err) throw err;
-            //console.log('resultmysql==', result.insertId);
-            //res.setHeader('Access-Control-Allow-Origin');
-           //res.json(result);
-           //return next();
+
            res.set('Content-Type', 'text/html')
-           console.log('rows.insertId------->', rows.insertId);
+
            res.json(rows.insertId);
         });
-       // res.end();
+    }
+
     });
     
-    router.post('/complete-task', (req, res) =>{
+    router.put('/complete-task', (req, res) =>{
         let idTask = req.body.key;
         let status = req.body.status;
         let sql = `UPDATE tasks SET status='${(status)}' WHERE tasksID='${(idTask)}'`;
         let query = db.query(sql, (err, result, fields) => {
             if (err) throw err;
-            console.log('result------->',result);
-            res.json(result);
+            //res.json(result);
         })
     });
+    router.delete('/delete-task', (req, res) => {
+        let idTask = req.body.key;
+        console.log('req.body.key==', idTask)
+        let sql = `DELETE FROM tasks WHERE tasksID IN (${(idTask)})`;
+        let query = db.query(sql, (err, result, fields) => {
+            if (err) throw err;
+        })
+    })
+    router.put('/edite-task', (req, res) => {
+        let idTask = req.body.key;
+        let textData = req.body.Title;
+        if((textData === 0 || textData.trim())){
+            console.log('textData', textData)
+            let sql = `UPDATE tasks SET Title='${(textData)}' WHERE tasksID='${(idTask)}'`;
+            let query = db.query(sql, (err, result, fields) => {
+                if (err) throw err;
+            })
+        }
+
+    })
 
     module.exports = router;
--- todo.sql
+++ todo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 30, 2019 at 10:00 PM
--- Server version: 5.6.41
--- PHP Version: 5.5.38
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 20 2019 г., 15:14
+-- Версия сервера: 5.7.20
+-- Версия PHP: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,108 +19,100 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `todo`
+-- База данных: `todo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projects`
+-- Структура таблицы `projects`
 --
 
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `user_id` int(10) NOT NULL
+  `Name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `projects`
+--
+
+INSERT INTO `projects` (`id`, `Name`) VALUES
+(4, 'Новый проект'),
+(5, 'Следующий проектuuuu'),
+(21, 'Nkhjkjj'),
+(22, 'New TODO LIyui'),
+(23, 'New TODO LIST1568967054866');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tasks`
+-- Структура таблицы `tasks`
 --
 
 CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
+  `tasksID` int(11) NOT NULL,
   `Title` varchar(50) NOT NULL,
   `status` varchar(10) NOT NULL,
   `project_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `users`
+-- Дамп данных таблицы `tasks`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `tasks` (`tasksID`, `Title`, `status`, `project_id`) VALUES
+(20, '        hjkhk', 'true', 4),
+(22, 'Задание 6ншншtuut', 'true', 5),
+(23, 'задание 76,,,,', 'false', 5),
+(42, 'задание 76', 'false', 5),
+(46, 'khk', 'false', 4),
+(48, ' n', 'false', 4),
+(49, ' jjj ', 'false', 4),
+(50, 'yuiy', 'false', 21);
 
 --
--- Indexes for dumped tables
+-- Индексы сохранённых таблиц
 --
 
 --
--- Indexes for table `projects`
+-- Индексы таблицы `projects`
 --
 ALTER TABLE `projects`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `project_id` (`project_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `projects`
---
-ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tasks`
+-- Индексы таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD PRIMARY KEY (`tasksID`),
+  ADD KEY `project_id` (`project_id`);
 
 --
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- Constraints for table `projects`
+-- AUTO_INCREMENT для таблицы `projects`
 --
 ALTER TABLE `projects`
-  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- Constraints for table `tasks`
+-- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
+  MODIFY `tasksID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
